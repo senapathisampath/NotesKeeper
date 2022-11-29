9e7221a336bdd20921921a203600fb7326fbb0b4
senapathi <senapathisampath@gmail.com>
2022-11-29 22:39:38 +0530

    new

---
+++ build/precache-manifest.a4395cdb96821ed286b2407c61ce28fb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af90fab5b12c07c9472913607721ffc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6cbe565dec355a98270",
+    "url": "/static/js/2.26daa70d.chunk.js"
+  },
+  {
+    "revision": "e6e9f51675857f0bda30",
+    "url": "/static/js/main.38e77472.chunk.js"
+  },
+  {
+    "revision": "2535e277f6499596031f",
+    "url": "/static/js/runtime-main.9c1af6e4.js"
+  }
+]);
