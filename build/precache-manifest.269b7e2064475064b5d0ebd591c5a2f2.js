19adecdaa4774871dad047062a33d5718de7436b
senapathi <senapathisampath@gmail.com>
2022-11-29 22:55:59 +0530

    link stle error

---
+++ build/precache-manifest.269b7e2064475064b5d0ebd591c5a2f2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a01b6bca8465bd54dce9b3b34c811779",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e6cbe565dec355a98270",
+    "url": "./static/js/2.26daa70d.chunk.js"
+  },
+  {
+    "revision": "e6e9f51675857f0bda30",
+    "url": "./static/js/main.38e77472.chunk.js"
+  },
+  {
+    "revision": "35585a1987cc82530a09",
+    "url": "./static/js/runtime-main.6436613b.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a4395cdb96821ed286b2407c61ce28fb.js"
+  "./precache-manifest.269b7e2064475064b5d0ebd591c5a2f2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
